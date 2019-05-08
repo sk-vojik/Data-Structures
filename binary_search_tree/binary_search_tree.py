c1c88356a22ec6c127e15b14475a212b529285f9
sk-vojik <scott.vojik@gmail.com>
2019-05-08 11:57:39 -0700

    halfway done with binary search

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,36 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # check if the new node's value is less than the current node's value
+    if value < self.value:
+      #check if there is no left child
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      # if there is a left, keep goin down
+      else:
+        self.left.insert(value)
+    # do the same on the right side if the node's value >= the current node's value
+    else:
+      #check if there is no right child
+      if not self.right:
+        #park value here
+        self.right = BinarySearchTree(value)
+      else:
+        #keep recursing down to the right since there is a right child
+        self.right.insert(value)
 
   def contains(self, target):
-    pass
+    if target == self.value:
+      return true
+    elif target < self.value:
+      if self.left is None:
+        return False
+      else:
+        return self.left.contains(target)
+
 
   def get_max(self):
-    pass
+    return self.value
 
   def for_each(self, cb):
     pass
